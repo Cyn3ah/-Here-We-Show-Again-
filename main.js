d5d27aa88ddd89505ffef7398c138358824e3b42
Cyn3ah <62955916+Cyn3ah@users.noreply.github.com>
2020-06-15 13:26:24 -0500

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-//This webpage is brought to you by Douglas Wilson, Safea Patrick Mansa-Musa
+//This webpage is brought to you by Douglas Wilson, Safea Patrick Mansa-Musa, Darius Chappell and myself (Cynthia Evans)
+
+//Kata 1
+
+
+
+//Kata 2
+
+
+
+//Kata 3
+
+
+
+//Kata 4
+
+
+
+//Kata 5
+
+
+
+//Kata 6
